cec003a4e5f69939ff196b8ca7227196c56a9f6f
gozgirfaruk <gozgirfaruk@gmail.com>
2024-07-26 15:01:14 +0300

    CarDetail sayfası yenilendi

--- DapperProject/Dtos/PlatesDtos/SearchResultDto.cs
+++ DapperProject/Dtos/PlatesDtos/SearchResultDto.cs
@@ -9,12 +9,12 @@
         public string Brand { get; set; }
         public string Model { get; set; }
         public DateTime LicenceDate { get; set; }
-        public string? Year { get; set; }
-        public string? Fuel { get; set; }
-        public string? ShiftType { get; set; }
-        public string? MotorVolume { get; set; }
-        public string? MotorPower { get; set; }
-        public string? Color { get; set; }
-        public string? CaseType { get; set; }
+        public string Year { get; set; }
+        public string Fuel { get; set; }
+        public string ShiftType { get; set; }
+        public string MotorVolume { get; set; }
+        public string MotorPower { get; set; }
+        public string Color { get; set; }
+        public string CaseType { get; set; }
     }
 }
